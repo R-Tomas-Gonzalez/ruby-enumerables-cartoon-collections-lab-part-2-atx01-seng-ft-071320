d7a10120cdd506388e0b89b56ecc549d2ee215c5
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-07-02 23:55:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |elements|
-    elements.include?
+  planeteer_calls.find.include? valid_calls do |elements|
+    elements
   end
 
 end
